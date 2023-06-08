868b32f6647445458aecd24357dd44369aed87e8
nikita parmar <98653385+Nikitaparmar04@users.noreply.github.com>
2023-06-08 15:31:56 +0530

    Update CeasarCipher.cpp

--- CeasarCipher.cpp
+++ CeasarCipher.cpp
@@ -1,5 +1,53 @@
 //
 // Created by NIKITA on 08-06-2023.
-//
+#include <iostream>
+using namespace std;
 
-#include "CeasarCipher.h"
+// Function to encrypt the message
+string encrypt(string message, int shift) {
+    string encryptedMessage = "";
+    for (int i = 0; i < message.length(); i++) {
+        if (isalpha(message[i])) {
+            char encryptedChar = (toupper(message[i]) - 'A' + shift) % 26 + 'A';
+            encryptedMessage += encryptedChar;
+        } else {
+            encryptedMessage += message[i];
+        }
+    }
+    return encryptedMessage;
+}
+
+// Function to decrypt the message
+string decrypt(string message, int shift) {
+    string decryptedMessage = "";
+    for (int i = 0; i < message.length(); i++) {
+        if (isalpha(message[i])) {
+            char decryptedChar = (toupper(message[i]) - 'A' - shift + 26) % 26 + 'A';
+            decryptedMessage += decryptedChar;
+        } else {
+            decryptedMessage += message[i];
+        }
+    }
+    return decryptedMessage;
+}
+
+int main() {
+    string message;
+    int shift;
+
+    // Read the message and shift value from the user
+    cout << "Enter the message: ";
+    getline(cin, message);
+    cout << "Enter the shift value: ";
+    cin >> shift;
+
+    // Encrypt the message
+    string encryptedMessage = encrypt(message, shift);
+    cout << "Encrypted message: " << encryptedMessage << endl;
+
+    // Decrypt the message
+    string decryptedMessage = decrypt(encryptedMessage, shift);
+    cout << "Decrypted message: " << decryptedMessage << endl;
+
+    return 0;
+}
